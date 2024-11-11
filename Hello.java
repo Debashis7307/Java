86bbf8337cb97e64238a87d8dc09361b6ad36cef
Debashis7307 <debashisalak@gmail.com>
2024-11-11 23:02:10 +0530

    just having start with java

--- Hello.java
+++
@@ -1,4 +0,0 @@
-public class Hello {
-  int number;
-  int year;
-}
---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+      System.out.println("Hello World");
+      //primetive types variables
+      int a = 54;
+      char ch ='D';
+      long b = 123456789L;
+      float  c = 1234567.89f;
+      double d = 1234567.89;
+      boolean isAdult = true;
+
+      //non-primitive types variables
+      String name = new String("Debashis");
+      System.out.print(name.length());
+  }
+}
